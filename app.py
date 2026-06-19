c169f1513825c99e91cc399a8084459525ca2356
UmutD34 <sperosar05@gmail.com>
2026-06-19 10:38:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def load_data():
 
 data = load_data()
 mevcut_dersler = data.get("dersler", {})
-tum_dersler = ["Türkçe", "Tarih", "Matematik", "Fen", "Sosyal"]
+tum_dersler = ["Türkçe", "Tarih", "Din kültürü ve Ahlak Bilgisi "(ekleniyor)" ", "...", "..."]
 
 if "hap_idx" not in st.session_state:
     st.session_state.hap_idx = 0
